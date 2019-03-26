0c8b98416130cf32c7972ca47bfc1069899f9dc8
Rich Posert <plethoraofchutneys@gmail.com>
2019-03-25 22:02:21 -0700

    Finally move away from Elise formatting. Less strict, more understandable

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import csv
 import os
 import sys
 import argparse
+import re
 try:
 	import bs4
 except:
@@ -17,32 +18,27 @@ except:
 #
 # When you're searching for job titles, try a few different things, because
 # whatever search they're doing sucks
-
-# Save the html files with the format '***_$of#.html', where *** is some string
-# of any length that identifies which job title you searched, # is the total
-# number of pages you downloaded, and $ is whatever the current page is
-# For instance, 'RA_1of10.html'
 #
-# If you want to change this format it's used in get_content() and you'll have
-# to update the argument parsing as well.
+# The html files need a unique identifier at the beginning
+
 
 
 # name_id is what identifies the `name` field in the O2 directory. Unless OHSU
 # changes the way smartweb is formatted, this shouldn't change
 name_id = 'Click to view more details on this directory record'
 
-# I'm assuming you've structured your directory like this, with union_shit being
-# whatever your parent directory is:
-# .../union_shit/scraper.py
-# .../union_shit/lists/*.html
-# Lists made with this script would, in this case, end up in .../union_shit
-path = os.getcwd()
-
-
 # Names are the grandchildren of the total entry; department is another grandchild
 # This pulls both and returns them in a list
 # I basically lucked out that the department is the only other <a> tag
 # For some reason checking against a dep_id didn't work
+def get_files(dir, indicator = '.html'):
+	list_of_files = []
+	regex = re.compile('^' + indicator)
+
+	for file in os.listdir(dir):
+		if file.endswith('.html') & (re.match(regex, file) is not None):
+			list_of_files.append(os.path.join(dir, file))
+	return list_of_files
 
 def find_dep(name):
 	parent = name.parent.parent
@@ -51,21 +47,15 @@ def find_dep(name):
 			dep = link.string
 	return [name.string, dep]
 
-def get_content(type, max, quiet):
-	content_list = []
-	num = []
-
-	if max > 1:
-		num = range(1,max+1)
-	else:
-		num = [1]
-
-	for i in num:
-		content_list.append(os.path.join(path,'lists',"%s_%iof%i.html" % (type, i, max)))
+def get_content(type, directory, quiet):
+	files = get_files(directory, type)
 
 	soups = []
-	for content in content_list:
-		soups.append(bs4.BeautifulSoup(open(content), "html.parser"))
+	for file in files:
+		if not quiet:
+			print(f'Reading {os.path.basename(file)}')
+
+		soups.append(bs4.BeautifulSoup(open(file), "html.parser"))
 
 	list_of_names = []
 	list_of_deps = []
@@ -79,7 +69,7 @@ def get_content(type, max, quiet):
 				list_of_deps.append(find_dep(name)[1])
 
 	if not quiet:
-		print(f'Found {len(list_of_names)} employees in {max} lists of {type} workers.')
+		print(f'Found {len(list_of_names)} employees in {len(files)} lists of {type} workers.')
 
 	with open("%s_names_deps.csv" % (type), 'w', newline = '') as file:
 			wr = csv.writer(file, quoting=csv.QUOTE_ALL)
@@ -89,12 +79,10 @@ def get_content(type, max, quiet):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = 'A simple script to analyze SmartWeb htmls saved as ###_{i}of{n}.html')
 	parser.add_argument('prefix', help = 'Prefix added to html files to be scraped')
-	parser.add_argument('number', help = 'Number of html files to scrape', type = int)
+	parser.add_argument('directory', help = 'Directory to look for lists in')
 	parser.add_argument('-q', '--quiet', help = 'Don\'t print number of employees found', action = 'store_true')
+	args = parser.parse_args()
 
 	if len(sys.argv) == 1:
-		parser.print_help(sys.stderr)
 		sys.exit(0)
-
-	args = parser.parse_args()
-	get_content(args.prefix, args.number, args.quiet)
+	get_content(args.prefix, args.directory, args.quiet)
